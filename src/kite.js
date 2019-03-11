1d52e3206f0512af73efa835e23b3a8c7572653b
frankymacster <frankymacster@users.noreply.github.com>
2019-03-11 18:10:38 -0400

    Update kite.js

--- src/kite.js
+++ src/kite.js
@@ -3,6 +3,10 @@ const curry = require('fantasy-helpers').curry
 
 //# kite :: a -> b -> b
 //.
+//. ```js
+//. > kite('bird')('cat')
+//. 'cat'
+//. ```
 const kite = curry((x, y) => y)
 
 module.exports = kite
